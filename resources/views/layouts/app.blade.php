49b08fcf22006b3c4f1c9face72fa2fb8c415f6b
MartijnvanderBruggen <martijnvanderbruggen@live.nl>
2020-11-23 17:30:51 +0000

    git was complaining

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,7 @@
         <title>App Name - @yield('title')</title>
     </head>
     <body>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
-
-        <div class="container">
+      <div class="container">
             @yield('content')
         </div>
     </body>
