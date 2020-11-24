824d02067f2265f084e2e834769c76a0b9670b5d
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-11-24 17:54:34 +0100

    dfa

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,12 +6,13 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 window.$ = window.jQuery = require('jquery');
+require('foundation-sites');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.foundation = require('foundation-sites');
 
-$(document).foundation();
+
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
