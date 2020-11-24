31c036c19d4fa32f3f80e1f616ab714e60bd1deb
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-11-24 16:15:25 +0100

    jquery

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,11 +5,13 @@ window._ = require('lodash');
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
+import $ from "jquery";
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.foundation = require('foundation-sites');
 
+$(document).foundation();
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
