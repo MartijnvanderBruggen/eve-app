04db83e333a52504c2bbeb8ff652bfe2e4de9b8b
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-11-24 19:01:25 +0100

    removed foundation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,18 +4,15 @@
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foundation Starter Template</title>
+    <title>Eve-app</title>
     <link rel="stylesheet" href="css/app.css" />
-    <link rel="stylesheet" href="css/foundation.css" />
+
 
   </head>
   <body>
     @yield('content')
+
     <script src="{{ mix('js/app.js') }}"></script>
-
-    <script src="js/vendor/what-input.js"></script>
-    <script src="js/vendor/foundation.min.js"></script>
-
     @stack('scripts')
   </body>
 </html>
