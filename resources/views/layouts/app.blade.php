4703464c1c2630825f340b4cae7ef5b60c284c58
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-11-24 16:32:58 +0100

    tstst

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,12 +12,10 @@
   <body>
     @yield('content')
     <script src="js/app.js"></script>
-    
+
     <script src="js/vendor/what-input.js"></script>
     <script src="js/vendor/foundation.min.js"></script>
-    <script>
-      $(document).foundation();
-    </script>
+    
     @stack('scripts')
   </body>
 </html>
