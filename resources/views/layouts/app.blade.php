9ebf6442f7720be3b490dd958238be78c07fd908
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-11-24 16:32:19 +0100

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,13 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Foundation Starter Template</title>
+    <link rel="stylesheet" href="css/app.css" />
     <link rel="stylesheet" href="css/foundation.css" />
 
   </head>
   <body>
     @yield('content')
-
+    <script src="js/app.js"></script>
     <script src="js/vendor/jquery.js"></script>
     <script src="js/vendor/what-input.js"></script>
     <script src="js/vendor/foundation.min.js"></script>
