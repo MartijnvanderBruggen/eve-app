bc9b9f7b123ad85196616f55abacc63a4b140467
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-11-24 23:23:45 +0100

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,14 +11,14 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends BaseController
 {
   public function index() {
-    // $user = Auth::user();
-    //
-    // $responseJSON = Http::withHeaders([
-    //   'Authorization' => 'Bearer '. $user->password,
-    //   ])->get('https://esi.evetech.net/latest/characters/'.$user->eve_id.'/assets/');
-    //
-    // $assets = collect(json_decode($responseJSON->body()));
-    //
-    return view('dashboard');
+    $user = Auth::user();
+
+    $responseJSON = Http::withHeaders([
+        'Authorization' => 'Bearer '. $user->password,
+    ])->get('https://esi.evetech.net/latest/characters/'.$user->eve_id.'/assets/');
+
+    $assets = collect(json_decode($responseJSON->body()));
+
+    return view('dashboard',['assets' => $assets]);
   }
 }
