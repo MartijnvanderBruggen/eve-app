3927164c524a4ab362184540e5ae86535500aa97
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-11-24 19:54:40 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Eve-app</title>
     <link rel="stylesheet" href="css/app.css" />
-
+    <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
 
   </head>
   <body>
